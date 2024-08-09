c62426257c95a775ed7765051881931f586efe33
Wes Brown <Wes Brown@DESKTOP-BBIAGFR>
2024-08-09 11:46:59 -0500

    Add project files.

---
+++ MyWebsite/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Navigation;
+
+namespace FreelanceCoderApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            // Open the link in the default browser
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = e.Uri.AbsoluteUri,
+                UseShellExecute = true
+            });
+            // Mark the event as handled
+            e.Handled = true;
+        }
+    }
+}
+
